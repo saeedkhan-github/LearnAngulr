569a589e2ac2103220f581d4ba91cb79641a8c63
saeedkhan-github <saeedullah321@gmail.com>
2019-02-13 15:13:39 +0500

    docker file added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,11 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  // tslint:disable-next-line:use-input-property-decorator
+  inputs: ['parentData'],
 })
 export class HeroesComponent implements OnInit {
 
   constructor() { }
+  public parentData: string;
 
   ngOnInit() {
   }
